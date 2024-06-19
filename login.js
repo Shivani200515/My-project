998c05d84c6272dc5317c91de0eda2247e8b7062
Shivani200515 <shivanivemula15@gmail.com>
2024-06-19 20:49:53 +0530

    Update login.js

--- login.js
+++ login.js
@@ -5,9 +5,13 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     var password = document.getElementById("password").value;
   
     if (username && password) {
+        if(password.length >=8) {
       document.getElementById("message").innerText = "Login successful!";
+    } else {
+           document.getElementById("message").innerText = "Password must be at least 8 characters.";
+            }
     } else {
       document.getElementById("message").innerText = "Please enter username and password.";
     }
   });
-  
+});
